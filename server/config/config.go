207be460e07c249532a7b42823f70f0da9062945
chadcarlson <chad.carlson@platform.sh>
2020-06-17 14:21:06 -0400

    Working posts, all but synonyms.

--- server/config/config.go
+++ server/config/config.go
@@ -1,13 +1,16 @@
 package config
 
 import (
-
+  "gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
 )
 
 type Config struct {
 	Index struct {
 		Name       string `yaml:"name"`
 		PrimaryKey string `yaml:"primaryKey"`
+    UID        string `yaml:"uid"`
 	} `yaml:"index"`
 	Attributes struct {
 		Displayed  []string `yaml:"displayed"`
@@ -24,4 +27,24 @@ type Config struct {
 			Regions     []string `yaml:"regions,omitempty"`
 		} `yaml:"list"`
 	} `yaml:"synonyms"`
+}
+
+// Parses local server config yaml.
+func (c *Config) ParseYAML(data []byte) error {
+	return yaml.Unmarshal(data, c)
+}
+
+// Load the local server config file.
+func (p Config) Load(file string) Config {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var config Config
+	if err := config.ParseYAML(data); err != nil {
+		log.Fatal(err)
+	}
+
+	return config
 }
--- server/server.go
+++ server/server.go
@@ -7,76 +7,120 @@ import (
     "log"
     "strconv"
     "github.com/meilisearch/meilisearch-go"
+    config "github.com/chadwcarlson/gomeili/server/config"
+    docs "github.com/chadwcarlson/gomeili/index/documents"
 )
 
-func deleteIndex(client *meilisearch.Client, indexUID string) {
+func Post(config config.Config, documents docs.Index) {
+
+    io.WriteString(os.Stdout, "\n\033[1mPosting index to Meilisearch...\033[0m\n\n")
+
+    // Define the Meilisearch client.
+    var client = meilisearch.NewClient(meilisearch.Config{
+        Host: "http://127.0.0.1:7700",
+    })
+
+    // Create the index.
+    createIndex(client, config)
+
+    // Update the settings.
+    updateSettings(client, config)
+
+    // Add the documents.
+    addDocuments(client, config, documents)
+
+}
+
+func createIndex(client *meilisearch.Client, config config.Config) {
+
+  // Delete the index if it already exists.
+  deleteIndex(client, config)
+
+  _, err := client.Indexes().Create(meilisearch.CreateIndexRequest{
+      UID: config.Index.UID,
+  })
+  handleErr(err)
+  io.WriteString(os.Stdout, fmt.Sprintf("- Index created.\n\n"))
+}
+
+func deleteIndex(client *meilisearch.Client, config config.Config) {
   numIndexes, err := client.Indexes().List()
   handleErr(err)
   // If index already exists for the indexUID, delete it.
   if len(numIndexes) > 0 {
-    _, err := client.Indexes().Delete(indexUID)
+    _, err := client.Indexes().Delete(config.Index.UID)
     handleErr(err)
     io.WriteString(os.Stdout, "- Previous index deleted.\n")
   }
 }
 
-func createIndex(client *meilisearch.Client, indexUID string) {
-  _, err := client.Indexes().Create(meilisearch.CreateIndexRequest{
-      UID: indexUID,
-  })
-  handleErr(err)
-  io.WriteString(os.Stdout, fmt.Sprintf("- Index created.\n"))
+func updateSettings(client *meilisearch.Client, config config.Config) {
+  // Update the index's name.
+  updateName(client, config)
+
+  // Set the primary search key.
+  updatePrimaryKey(client, config)
+
+  // Update synonyms.
+  updateSynonyms(client, config)
+
+  // Update ranking rules.
+  updateRankingRules(client, config)
+
+  // Update searchable attributes.
+  updateSearchableAttributes(client, config)
+
+  // Update displayed attributes.
+  updateDisplayedAttributes(client, config)
 }
 
-func updateSynonyms(client *meilisearch.Client, indexUID string) {
+func updateName(client *meilisearch.Client, config config.Config) {
+  _, err := client.Indexes().UpdateName(config.Index.UID, config.Index.Name)
+  handleErr(err)
+}
+
+func updatePrimaryKey(client *meilisearch.Client, config config.Config) {
+  _, err := client.Indexes().UpdatePrimaryKey(config.Index.UID, config.Index.PrimaryKey)
+  handleErr(err)
+}
+
+func updateSynonyms(client *meilisearch.Client, config config.Config) {
   synonyms := map[string][]string{
     "hobbit": []string{"automobiles"},
     "automobiles": []string{"hobbit"},
   }
 
-  updateIDRes, err := client.Settings(indexUID).UpdateSynonyms(synonyms)
+  updateIDRes, err := client.Settings(config.Index.UID).UpdateSynonyms(synonyms)
   handleErr(err)
-  io.WriteString(os.Stdout, fmt.Sprintf("- %s: Synonyms applied.\n", strconv.FormatInt(updateIDRes.UpdateID, 10) ))
+  io.WriteString(os.Stdout, fmt.Sprintf("%s - Synonyms applied.\n", strconv.FormatInt(updateIDRes.UpdateID, 10) ))
 }
 
-func updateRankingRules(client *meilisearch.Client, indexUID string) {
-  rankingRules := []string{"typo", "words", "proximity", "attribute", "wordsPosition", "exactness"}
+func updateRankingRules(client *meilisearch.Client, config config.Config) {
 
-  updateIDRes, err := client.Settings(indexUID).UpdateRankingRules(rankingRules)
+  updateIDRes, err := client.Settings(config.Index.UID).UpdateRankingRules(config.RankingRules)
   handleErr(err)
-  io.WriteString(os.Stdout, fmt.Sprintf("- %s: Ranking rules updated.\n", strconv.FormatInt(updateIDRes.UpdateID, 10) ))
+  io.WriteString(os.Stdout, fmt.Sprintf("%s - Ranking rules updated.\n", strconv.FormatInt(updateIDRes.UpdateID, 10) ))
 }
 
-func updateSearchableAttributes(client *meilisearch.Client, indexUID string) {
-  searchableAttributes := []string{"id", "title", "description"}
+func updateSearchableAttributes(client *meilisearch.Client, config config.Config) {
 
-  updateIDRes, err := client.Settings(indexUID).UpdateSearchableAttributes(searchableAttributes)
+  updateIDRes, err := client.Settings(config.Index.UID).UpdateSearchableAttributes(config.Attributes.Searchable)
   handleErr(err)
-  io.WriteString(os.Stdout, fmt.Sprintf("- %s: Searchable attributes updated.\n", strconv.FormatInt(updateIDRes.UpdateID, 10) ))
+  io.WriteString(os.Stdout, fmt.Sprintf("%s - Searchable attributes updated.\n", strconv.FormatInt(updateIDRes.UpdateID, 10) ))
 }
 
-func updateDisplayedAttributes(client *meilisearch.Client, indexUID string) {
-  displayedAttributes := []string{"id", "title", "description"}
+func updateDisplayedAttributes(client *meilisearch.Client, config config.Config) {
 
-  updateIDRes, err := client.Settings(indexUID).UpdateDisplayedAttributes(displayedAttributes)
+  updateIDRes, err := client.Settings(config.Index.UID).UpdateDisplayedAttributes(config.Attributes.Displayed)
   handleErr(err)
-  io.WriteString(os.Stdout, fmt.Sprintf("- %s: Displayed attributes updated.\n", strconv.FormatInt(updateIDRes.UpdateID, 10) ))
+  io.WriteString(os.Stdout, fmt.Sprintf("%s - Displayed attributes updated.\n", strconv.FormatInt(updateIDRes.UpdateID, 10) ))
 }
 
-func addDocuments(client *meilisearch.Client, indexUID string) {
-  documents := []map[string]interface{}{
-      {"book_id": 123,  "title": "Pride and Prejudice"},
-      {"book_id": 456,  "title": "Le Petit Prince"},
-      {"book_id": 1,    "title": "Alice In Wonderland"},
-      {"book_id": 1344, "title": "The Hobbit"},
-      {"book_id": 4,    "title": "Harry Potter and the Half-Blood Prince"},
-      {"book_id": 42,   "title": "The Hitchhiker's Guide to the Galaxy"},
-      {"book_id": 45,   "title": "Planes, Trains, and Automobiles"},
-  }
+func addDocuments(client *meilisearch.Client, config config.Config, documents docs.Index) {
 
-  updateIDRes, err := client.Documents(indexUID).AddOrUpdate(documents)
+  updateIDRes, err := client.Documents(config.Index.UID).AddOrUpdate(documents.Documents)
   handleErr(err)
-  io.WriteString(os.Stdout, fmt.Sprintf("- %s: Adding documents.\n", strconv.FormatInt(updateIDRes.UpdateID, 10) ))
+  io.WriteString(os.Stdout, fmt.Sprintf("%s - Adding documents.\n", strconv.FormatInt(updateIDRes.UpdateID, 10) ))
 }
 
 func handleErr(err error) {
@@ -84,38 +128,4 @@ func handleErr(err error) {
       log.Fatal(err)
       os.Exit(1)
   }
-}
-
-func Post() {
-
-    io.WriteString(os.Stdout, "\n\033[1mPosting index to Meilisearch...\033[0m\n")
-
-    indexUID := "books"
-
-    // Define the Meilisearch client.
-    var client = meilisearch.NewClient(meilisearch.Config{
-        Host: "http://127.0.0.1:7700",
-    })
-
-    // Delete the index if it already exists.
-    deleteIndex(client, indexUID)
-
-    // Create an index if your index does not already exist
-    createIndex(client, indexUID)
-
-    // Update synonyms.
-    updateSynonyms(client, indexUID)
-
-    // Update ranking rules.
-    updateRankingRules(client, indexUID)
-
-    // Update searchable attributes.
-    updateSearchableAttributes(client, indexUID)
-
-    // Update displayed attributes.
-    updateDisplayedAttributes(client, indexUID)
-
-    // Add documents.
-    addDocuments(client, indexUID)
-
 }
